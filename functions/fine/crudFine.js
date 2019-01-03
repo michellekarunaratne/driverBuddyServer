9864dd6191b2b1f8456b291c4b01a49d97606a93
Michelle <msumalini@gmail.com>
2019-01-03 20:20:39 +0530

    added nodemailer module and created sending email function

--- functions/fine/crudFine.js
+++ functions/fine/crudFine.js
@@ -2,6 +2,7 @@ const Driver=require('../../models/driver');
 const Police=require('../../models/police');
 const SpotFine=require('../../models/spotFine');
 const FineTicket=require('../../models/fineTicket');
+const nodemailer = require('nodemailer');
 
 
 const details={}
@@ -149,8 +150,48 @@ function findFineTicketPolice(policeId)
     return promise;
 }
 
+function sendEmail(email)
+{
+    nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // true for 465, false for other ports
+            auth: {
+                user:"driverbuddyemail@gmail.com",
+                pass:"driverbuddy_1" 
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"No Reply" <driverbuddyemail@gmail.com>', // sender address
+            to: email, // list of receivers
+            subject: 'Payement Reciept', // Subject line
+            text: 'Your fine has been successfully paid,Thank you for using driver buddy', // plain text body
+            html: '<b>SUCCESSFUL</b><p>Your fine has been successfully paid,Thank you for using driver buddy</p>' // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        });
+    });
+}
+
+
 
 
 module.exports.enterFineDetails=enterFineDetails;
 module.exports.findFineTicketDriver=findFineTicketDriver;
 module.exports.findFineTicketPolice=findFineTicketPolice;
+module.exports.sendEmail=sendEmail;
+
